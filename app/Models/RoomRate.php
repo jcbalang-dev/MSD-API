58ca0de924d28ddb432c431157be8a4bf8defebd
John Christian Balang <dev.jcbalang@gmail.com>
2024-05-01 17:13:18 +0800

    connect table - model - room rate

--- app/Models/RoomRate.php
+++ app/Models/RoomRate.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class RoomRate extends Model
 {
     use HasFactory;
+    
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'room_rate';
+    
 }
