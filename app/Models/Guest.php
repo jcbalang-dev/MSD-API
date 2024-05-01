d99b753409dd5d9b2fcdd380f0769d58277657d5
John Christian Balang <dev.jcbalang@gmail.com>
2024-05-01 17:12:02 +0800

    connect table - model - guest

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Guest extends Model
 {
     use HasFactory;
+    
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'guest';
+
 }
