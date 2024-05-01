70d7c3f729f033be068b29a0f2bfeed006cc8ca1
John Christian Balang <dev.jcbalang@gmail.com>
2024-05-01 17:12:58 +0800

    connect table - model - room bedspace

--- app/Models/RoomBedspace.php
+++ app/Models/RoomBedspace.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class RoomBedspace extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'room_bedspace';
+
 }
