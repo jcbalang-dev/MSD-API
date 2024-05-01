af151b37c6c4cb53f1fece5dc32451066a6dc57e
John Christian Balang <dev.jcbalang@gmail.com>
2024-05-01 17:12:34 +0800

    connect table - model - room

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+    */
+    protected $table = "room";
+
 }
